0ff5afe9f8c96baf09ddaa3f728fea52e3996db0
Bennett Liyanto <bliyanto@umich.edu>
2024-11-06 19:40:45 -0500

    newer version langchain

--- langchain/main.py
+++ langchain/main.py
@@ -1,4 +1,4 @@
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.agents import initialize_agent, AgentType
 from tools import tools
 import os
