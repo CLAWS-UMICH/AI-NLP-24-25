816e64cc6e08c01f57d0118f4af191de0c2d2563
Bennett Liyanto <bliyanto@umich.edu>
2024-11-06 19:39:12 -0500

    json version

--- langchain/main.py
+++ langchain/main.py
@@ -1,20 +1,21 @@
-# main.py
-
-from langchain import OpenAI
-from langchain.agents import initialize_agent
-from tools import tools 
+from langchain_community.chat_models import ChatOpenAI
+from langchain.agents import initialize_agent, AgentType
+from tools import tools
 import os
-from langchain_community.llms import OpenAIChat
 
-
-llm = OpenAI(model_name="gpt-4o")
-agent = initialize_agent(tools=tools, llm=llm, agent_type="zero-shot-react-description")
+llm = ChatOpenAI(model_name="gpt-4")
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=False
+)
 
 def langchain_agent_response(query: str):
-    response = agent.run(query)
+    response = agent.invoke(query)
     return response
 
 # Example usage
 if __name__ == "__main__":
-    response = input("Astronaut prompt: ")
-    print(langchain_agent_response(response))
+    prompt = input("Astronaut prompt: ")
+    print(langchain_agent_response(prompt))
